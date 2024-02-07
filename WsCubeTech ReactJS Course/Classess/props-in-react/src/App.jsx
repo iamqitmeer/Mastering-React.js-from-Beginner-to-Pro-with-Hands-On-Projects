432b5d597d261d50149c6e2101c0a6437e46dc98
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-02-07 17:31:44 +0500

    JSX

---
+++ WsCubeTech ReactJS Course/Classess/props-in-react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Component/Button'
+import Heading from './Component/Heading'
+
+const App = () => {
+  return (
+    <div>
+      <Heading title='Hello World' />
+      <Button title='click hua!' />
+    </div>
+  )
+}
+
+export default App
