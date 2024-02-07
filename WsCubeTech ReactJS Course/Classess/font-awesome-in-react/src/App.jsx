8aaa42a50e0886152128a528937d09677f32bd11
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-02-07 20:11:06 +0500

    font awesome in react

---
+++ WsCubeTech ReactJS Course/Classess/font-awesome-in-react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      {/* You can use any other Font Awesome icons similarly */}
+      <i class="fa-brands fa-facebook"></i>
+      <i class="fa-brands fa-apple"></i>
+    </div>
+  )
+}
+
+export default App
