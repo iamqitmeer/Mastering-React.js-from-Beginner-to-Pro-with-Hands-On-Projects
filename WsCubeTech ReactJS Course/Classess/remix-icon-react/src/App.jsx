521dc47a4e75a8a0cb4a7b2d40d6fa3f734fd967
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-02-08 01:43:17 +0500

    remix icom

---
+++ WsCubeTech ReactJS Course/Classess/remix-icon-react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { IconName } from '@remixicons/react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<i class="ri-admin-line"></i>
+<i class="ri-admin-fill"></i>    </>
+  )
+}
+
+export default App
